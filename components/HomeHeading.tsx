e3bc3a5b3d4b0e32e7b840d5491f033fd20818eb
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-11 01:40:12 +0900

    add google font link & home heading component

---
+++ components/HomeHeading.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/components/HomeHeading.module.scss';
+
+const HomeHeading = () => {
+    return (
+        <h1 className={styles.homeHeading}>shushuTona - Wolrd</h1>
+    )
+}
+
+export { HomeHeading };
