6d6b5a05039091a5ee34f2bd6908d05d7f921311
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-11 01:38:26 +0900

    change file path root

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const path = require( 'path' )
+
 module.exports = {
+  webpack ( config, options ) {
+    config.resolve.alias[ '@' ] = path.join( __dirname, './' )
+    return config
+  },
   reactStrictMode: true,
 }
