d78e8e314b8403913b94114f6af89b8cc11badfa
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-10 21:58:36 +0900

    init file

---
+++ pages/about/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../../styles/Home.module.css'
+
+const About = () => {
+    return (
+        <div className={styles.container}>
+            <p>About</p>
+        </div>
+    )
+}
+
+export default About
