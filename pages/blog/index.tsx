d99e4addc8abf5282ff345cb5ae04c4cb22c4402
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-12 01:32:19 +0900

    create Blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import { PageLayout } from '@/components/PageLayout';
+
+const Blog = () => {
+    return (
+        <PageLayout headingText="Blog">
+            <p>Blog</p>
+        </PageLayout>
+    )
+}
+
+export default Blog;
