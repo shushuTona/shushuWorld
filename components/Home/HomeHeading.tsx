7cd7c765658229ea5bb9f18bc44a0dfa8d8fcad3
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-12 01:31:24 +0900

    create About page

--- components/Home/HomeHeading.tsx
+++ components/Home/HomeHeading.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/components/Home/HomeHeading.module.scss';
 
 const HomeHeading = () => {
     return (
-        <h1 className={styles.homeHeading}>shushuTona - Wolrd</h1>
+        <h1 className={styles.homeHeading}>shushuTona - World</h1>
     )
 }
 
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,26 +1,40 @@
-import { PageLayout } from '@/components/pageLayout';
+import { PageLayout } from '@/components/PageLayout';
 
 import styles from '@/styles/pages/About.module.scss';
 
 const About = () => {
+    const aboutItem = [
+        {
+            title: '2021年1月 ～ 現在',
+            contents: '新宿の事業会社でWebエンジニアとして勤務しています。'
+        },
+        {
+            title: '2018年4月 ～ 2020年12月',
+            contents: '新宿のWeb制作会社でフロントエンドエンジニアとして勤務していました。企業のコーポレートサイトのリニューアル案件を主に扱っていました。'
+        },
+    ];
+
     return (
         <PageLayout headingText="About">
             <div>
-                <h2>自己紹介</h2>
-                <p>現在都内の事業会社でwebエンジニアとして働いています。</p>
+                <h2 className={styles.heading}>自己紹介</h2>
+                <p>現在都内の事業会社でWebエンジニアとして働いています。</p>
                 <p>Webアプリケーション開発が好きです。</p>
 
-                <h2>職務経歴</h2>
-                <p>大学4年生の就職活動時にWeb開発の勉強を始めて、以下の経歴で生きてきました。</p>
-                <dl>
-                    <div>
-                        <dt>2018年4月～2020年12月</dt>
-                        <dd>新宿のweb制作会社でフロントエンドエンジニアとして勤務していました。企業のコーポレートサイトのリニューアル案件を主に扱っていました。</dd>
-                    </div>
-                    <div>
-                        <dt>2021年1月～</dt>
-                        <dd>新宿の事業会社でwebエンジニアとして勤務しています。</dd>
-                    </div>
+                <h2 className={styles.heading}>職務経歴</h2>
+                <p>大学4年生の就職活動時にWeb開発の勉強を始めて、Web業界を志望しました。</p>
+
+                <dl className={styles.aboutList}>
+                    {
+                        aboutItem.map( ( item ) => {
+                            return (
+                                <div key={item.title} className={styles.aboutList__item}>
+                                    <dt className={styles.aboutList__title}>{ item.title }</dt>
+                                    <dd className={styles.aboutList__contents}>{ item.contents }</dd>
+                                </div>
+                            )
+                        })
+                    }
                 </dl>
             </div>
         </PageLayout>
