8bfb7fcc1edf0067c515063e257a70a6281054fd
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-23 22:50:47 +0900

    add heading component isPostPage option

--- components/PageHeading.tsx
+++ components/PageHeading.tsx
@@ -3,11 +3,14 @@ import styles from '@/styles/components/PageHeading.module.scss';
 
 type Props = {
     headingText: string
+    isPostPage?: boolean
 }
 
-const PageHeading: VFC<Props> = ( { headingText } ) => {
+const PageHeading: VFC<Props> = ( { headingText, isPostPage } ) => {
+    const headingClassName = isPostPage ? styles.postPageHeading : styles.pageHeading;
+
     return (
-        <h1 className={styles.pageHeading}>{ headingText }</h1>
+        <h1 className={headingClassName}>{ headingText }</h1>
     )
 }
 
