a13f499f22c14fb2a64c68c891f1ab7a21abd75d
shushuTona <42406967+shushuTona@users.noreply.github.com>
2022-04-25 21:38:17 +0900

    create post page side menu

--- components/blog/PostPageLayout.tsx
+++ components/blog/PostPageLayout.tsx
@@ -1,5 +1,5 @@
-import Head from "next/head";
-import { VFC } from 'react';
+import Head from 'next/head';
+import { VFC, useState, useEffect, useMemo } from 'react';
 
 import { PostItem } from '@/lib/api';
 import { Header } from '@/components/Header';
@@ -13,7 +13,43 @@ type Props = {
     post: PostItem,
 }
 
+type AnchorItem = {
+    tag: string;
+    anchorLink: string;
+    contents: string;
+}
+
 const PostPageLayout: VFC<Props> = ( { post } ) => {
+    const headingElemList = useMemo( () => {
+        return post.content.match( /<h[2|3|4|5|6] id=".+">.+<\/h[2|3|4]>/g ) || [];
+    }, [post.content] );
+
+    const [anchorLinkList, setAnchorLinkList] = useState<AnchorItem[]>( [] );
+
+    // 見出し部分のHTML文字列を基にアンカーリンクの一覧を生成する
+    useEffect( () => {
+        const anchorItemList: AnchorItem[] = headingElemList.map( ( headingElemStr ) => {
+            const elem = document.createElement( 'div' );
+            elem.innerHTML = headingElemStr;
+
+            const headingElem = elem.children[0];
+
+            const tag = headingElem.tagName.toLowerCase();
+            const id = headingElem.id;
+            const contents = headingElem.textContent || '';
+
+            const anchorItem: AnchorItem = {
+                tag,
+                anchorLink: location.pathname + '#' + id,
+                contents,
+            }
+
+            return anchorItem;
+        } );
+
+        setAnchorLinkList( anchorItemList );
+    }, [headingElemList, setAnchorLinkList] );
+
     return (
         <div className={styles.postPageLayout}>
             <Head>
@@ -23,7 +59,7 @@ const PostPageLayout: VFC<Props> = ( { post } ) => {
             <Header />
 
             <main className={styles.postPageLayout__main}>
-                <p className={styles.postPageLayout__date}>{ post.date }</p>
+                <p className={styles.postPageLayout__date}>{post.date}</p>
                 <PageHeading headingText={post.title} isPostPage={true} />
 
                 <div className={styles.postPageLayout__contents}>
@@ -36,7 +72,22 @@ const PostPageLayout: VFC<Props> = ( { post } ) => {
                     </article>
 
                     <aside className={styles.postPageLayout__side}>
-                        <p>見出しの一覧</p>
+                        <ul className={styles.postPageLayout__sideList}>
+                            {
+                                anchorLinkList && anchorLinkList.map( ( item: AnchorItem ) => {
+                                    return (
+                                        <li
+                                            className={( item.tag === 'h2' ) ? styles.postPageLayout__listItem : styles.postPageLayout__listChildItem}
+                                            key={item.anchorLink}
+                                        >
+                                            <a href={item.anchorLink}>
+                                                {item.contents}
+                                            </a>
+                                        </li>
+                                    )
+                                } )
+                            }
+                        </ul>
                     </aside>
                 </div>
             </main>
